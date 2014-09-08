3f53bcbe1281b17694f769c18a1cc5984dada7db
Håvard Fossli <hfossli@gmail.com>
2014-09-08 13:30:30 +0200

    License

--- FBTweak/Cells/_FBTweakTableViewIntegerCell.h
+++ FBTweak/Cells/_FBTweakTableViewIntegerCell.h
@@ -1,6 +1,11 @@
-//
-// Created by Agens AS for FBTweak on 03.09.14.
-//
+/**
+ Copyright (c) 2014-present, Facebook, Inc.
+ All rights reserved.
+ 
+ This source code is licensed under the BSD-style license found in the
+ LICENSE file in the root directory of this source tree. An additional grant
+ of patent rights can be found in the PATENTS file in the same directory.
+ */
 
 #import "_FBTweakTableViewCell.h"
 #import "FBIntegerTweak.h"
